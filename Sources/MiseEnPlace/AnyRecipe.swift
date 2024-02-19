8a2a00c5e0ccf434448a8d096c9bc02f9f4eefa5
Richard Piazza <richard@richardpiazza.com>
2024-02-19 15:48:55 -0800

    Generic Entities (#6)
    Any* Entities

---
+++ Sources/MiseEnPlace/AnyRecipe.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+public struct AnyRecipe: Recipe {
+    public var uuid: UUID
+    public var creationDate: Date
+    public var modificationDate: Date
+    public var name: String?
+    public var commentary: String?
+    public var classification: String?
+    public var imagePath: String?
+    public var amount: Double
+    public var unit: MeasurementUnit
+    public var formulaElements: [FormulaElement]
+    public var procedureElements: [ProcedureElement]
+    
+    public init(
+        uuid: UUID = UUID(),
+        creationDate: Date = Date(),
+        modificationDate: Date = Date(),
+        name: String? = nil,
+        commentary: String? = nil,
+        classification: String? = nil,
+        imagePath: String? = nil,
+        amount: Double = 1.0,
+        unit: MeasurementUnit = .noUnit,
+        formulaElements: [FormulaElement] = [],
+        procedureElements: [ProcedureElement] = []
+    ) {
+        self.uuid = uuid
+        self.creationDate = creationDate
+        self.modificationDate = modificationDate
+        self.name = name
+        self.commentary = commentary
+        self.classification = classification
+        self.imagePath = imagePath
+        self.amount = amount
+        self.unit = unit
+        self.formulaElements = formulaElements
+        self.procedureElements = procedureElements
+    }
+    
+    public init(_ recipe: Recipe) {
+        self.uuid = recipe.uuid
+        self.creationDate = recipe.creationDate
+        self.modificationDate = recipe.modificationDate
+        self.name = recipe.name
+        self.commentary = recipe.commentary
+        self.classification = recipe.classification
+        self.imagePath = recipe.imagePath
+        self.amount = recipe.amount
+        self.unit = recipe.unit
+        self.formulaElements = recipe.formulaElements.map { AnyFormulaElement($0) }
+        self.procedureElements = recipe.procedureElements.map { AnyProcedureElement($0) }
+    }
+}
